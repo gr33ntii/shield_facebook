31d152bebdda4c9699d9ad68e4a7f93a93ad5ea0
An Xuan Phan <gr33ntii@8chan.co>
2018-05-12 14:04:07 +0000

    fix request

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const request = require("request");
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const mainshield = require('./shieldstuff.js')
 
 var urlencodedParser = bodyParser.urlencoded({
