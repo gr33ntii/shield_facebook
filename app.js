c634dd36488f8d267ec055431261186cd9b3b1ca
An Xuan Phan <gr33ntii@8chan.co>
2018-05-12 13:23:50 +0000

    Up Project

---
+++ app.js
@@ -0,0 +1,58 @@
+const request = require("request");
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser')
+const mainshield = require('./shieldstuff.js')
+
+var urlencodedParser = bodyParser.urlencoded({
+        extended: false
+})
+
+
+
+app.get('/', (req, res) => res.send("<head><meta charset='utf-8'/><title>ZmLhZGDlYwLYwVZD==</title></head><center>Gr33ntii Server Is Protected, privately </br> Want to get Mail Server paid 25$  </br>Need contact me ? </br> xolzsec@8chan[dot]co or gr33ntii@gmail[dot]com </br> <a href='https://facebook.com/0x80f700'> Facebook Account </a> </center>"));
+
+
+app.get('/khienfb', (req, res) => {
+        res.sendFile(__dirname + "/shield/index.html", (err) => {
+                    if (err) {
+                                    console.log(err)
+                                }
+
+                })
+});
+
+app.post('/khienfb/', urlencodedParser, (req, res) => {
+        var token = req.body.token;
+        if (!token) {
+                    res.send("<script>alert('Không Để Trống Token')\nwindow.location.replace('/khienfb');</script>");
+                    return;
+
+                } else if (token) {
+                            mainshield.tokenchecker(token).then((result) => {
+                                            var userid = result.id;
+                                            if (!userid) {
+                                                                res.send("<script>alert('Token không hợp lệ')\nwindow.location.replace('/khienfb');</script>");
+                                                                return;
+                                                            } else {
+                                                                                mainshield.makeshield(token, userid).then((result) => {
+
+                                                                                                        var checkshield = result.data.is_shielded_set.is_shielded;
+                                                                                                        if (checkshield == true) {
+                                                                                                                                    res.send("<script>alert('Đã Bật Khiên Vui Lòng Check Facebook Bạn')\nwindow.location.replace('/khienfb');</script>");
+                                                                                                                                    return;
+
+                                                                                                                                } else {
+                                                                                                                                                            res.send("<script>alert('Lỗi Vui Lòng Báo Lại Cho Admin')\nwindow.location.replace('https://www.facebook.com/0x80f700');</script>");
+                                                                                                                                                            return;
+                                                                                                                                                        }
+
+                                                                                                    })
+
+                                                                            }
+
+                                        })
+
+                        }
+});
+app.listen(80, () => console.log("Connected!"))
